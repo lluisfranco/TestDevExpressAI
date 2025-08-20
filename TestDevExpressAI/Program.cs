d78f0613345e50130f3e5e65371128709d525bfa
Lluis Franco <hello@lluisfranco.com>
2025-08-20 15:39:45 +0200

    key

--- TestDevExpressAI/Program.cs
+++ TestDevExpressAI/Program.cs
@@ -11,10 +11,7 @@ namespace TestDevExpressAI
         /// </summary>
         [STAThread]
         static void Main()
-        {
-            var key = "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";
-
-            DevExpressAIHelper.Initialize(key);            
+        {                    
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
